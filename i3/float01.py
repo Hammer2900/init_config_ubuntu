1336342e9b59fcf68551361e32c6b00fca3b0acc
Jeks <evgeny2900@gmail.com>
2019-10-07 12:55:53 +0300

    new clean settings

---
+++ i3/float01.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env /home/izot/.local/share/virtualenvs/test_python3-_8u9lZwJ/bin/python
+import os
+
+import i3ipc
+
+i3 = i3ipc.Connection()
+
+
+def center_window():
+    """
+    Float and center window, i3wm.
+    :return:
+    """
+    a = i3.get_tree().find_focused()
+    if a.floating == 'user_on':
+        a.command('floating disable')
+        return
+    workspace_name = a.workspace().name
+    rect = a.workspace().rect
+    a.command('floating enable')
+    new_window_width = rect.width / 2
+    window_position = new_window_width / 2
+    a.command(f'move position {window_position:.0f} {rect.y + 25}')
+    a.command(f'resize set {new_window_width:.0f} {rect.height - 50}')
+    a.command(f'move container to workspace {workspace_name}')
+    # a.command(f'[con_id="{a.id}"] resize set 50 ppt 50 ppt')
+    a.command(f'[con_id="{a.id}"] focus')
+    a.command(f'workspace {workspace_name}')
+    # os.system(f'xdotool mousemove --window {a.id} 0 0')
+
+
+center_window()
