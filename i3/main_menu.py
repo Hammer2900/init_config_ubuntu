07a665fce38300b1edf355c747e3525bfa21a315
Tsybulskyi Evhenyi <etsy2900@gmail.com>
2023-01-18 12:08:42 +0200

    rofi menu

---
+++ i3/main_menu.py
@@ -0,0 +1,156 @@
+#!/run/media/izot/652aebd5-b153-4f9e-ba3d-2fb1b4d4b246/jek/TEMP/all_python/.venv_f/bin/python
+import re
+import subprocess
+
+import fire
+from rofi import Rofi
+
+
+class RofiCommands:
+    """Command class for control some actions.
+
+    pip install python-rofi
+    """
+    def __init__(self):
+        self._r = Rofi(fullscreen=False, exit_hotkeys=('Alt+F4', 'Control+q'))
+
+    def _cmd(self, commands: list):
+        return subprocess.check_output(commands).decode('utf-8')
+
+    def _cmd_background(self, commands: list):
+        return subprocess.Popen(commands)
+
+    def _find_all_sinks(self):
+        status_str = self._cmd(['wpctl', 'status'])
+        start = status_str.index('Sinks:')
+        end = status_str.index('Sink endpoints:')
+        index_ = status_str[start:end]
+        return {name: id.replace('.', '') for id, name in re.findall(r'(\d+.) (.+)', index_) if 'Radeon' not in name}
+
+    def _switch_sinks(self, id: str):
+        return self._cmd(['wpctl', 'set-default', id])
+
+    def _monitor_layout_3(self):
+        self._cmd(
+            [
+                'xrandr',
+                '--output',
+                'DisplayPort-0',
+                '--primary',
+                '--mode',
+                '1920x1080',
+                '--pos',
+                '1920x0',
+                '--rotate',
+                'normal',
+                '--output',
+                'DisplayPort-1',
+                '--off',
+                '--output',
+                'DisplayPort-2',
+                '--mode',
+                '1920x1080',
+                '--pos',
+                '3849x0',
+                '--rotate',
+                'normal',
+                '--output',
+                'HDMI-A-0',
+                '--mode',
+                '1920x1080',
+                '--pos',
+                '0x0',
+                '--rotate',
+                'normal',
+                '--output',
+                'DVI-D-0',
+                '--off',
+            ]
+        )
+
+    def _monitor_layout_2(self):
+        self._cmd(
+            [
+                'xrandr',
+                '--output',
+                'DisplayPort-0',
+                '--primary',
+                '--mode',
+                '1920x1080',
+                '--pos',
+                '0x0',
+                '--rotate',
+                'normal',
+                '--output',
+                'DisplayPort-1',
+                '--off',
+                '--output',
+                'DisplayPort-2',
+                '--mode',
+                '1920x1080',
+                '--pos',
+                '1920x0',
+                '--rotate',
+                'normal',
+                '--output',
+                'HDMI-A-0',
+                '--off',
+                '--output',
+                'DVI-D-0',
+                '--off',
+            ]
+        )
+
+    def vlc_show_menu(self):
+        name = self._r.text_entry('Paste url or magnet: ')
+        if 'youtube' in name:
+            self._cmd_background(['vlc', name])
+        elif 'magnet' in name:
+            self._cmd_background(['vlc', f'"{name}"'])
+
+    def show_i3_config_menu(self):
+        self._cmd_background(['gedit', '/home/izot/.config/i3/config'])
+
+    def sound_output_menu(self):
+        dict_sinks = self._find_all_sinks()
+        options = list(dict_sinks)
+        index, key = self._r.select('Please choice sinks', options)
+        # self._switch_sinks(dict_sinks[options[index]])
+        if index != -1:
+            self._switch_sinks(dict_sinks[options[index]])
+            self.sound_output_menu()
+        else:
+            print('[empty]', )
+
+    def monitor_output_menu(self):
+        dict_sinks = {'3 monitors': self._monitor_layout_3, '2 monitors': self._monitor_layout_2}
+        options = list(dict_sinks)
+        index, key = self._r.select('Please choice sinks', options)
+        if index != -1:
+            dict_sinks[options[index]]()
+            self.monitor_output_menu()
+        else:
+            print('[empty]')
+
+    def start_menu(self):
+        actions_list = {
+            'sound output': self.sound_output_menu,
+            'monitor output': self.monitor_output_menu,
+            'rztk parse': self.sound_output_menu,
+            'mute': self.sound_output_menu,
+            'ssh': self.sound_output_menu,
+            'vlc': self.vlc_show_menu,
+            'i3 config': self.show_i3_config_menu,
+        }
+        options = list(actions_list)
+        index, key = self._r.select('Please choice action', options)
+        print('[-]', index, key)
+        print('[-]', actions_list[options[index]])
+        if index != -1:
+            actions_list[options[index]]()
+        else:
+            print('[empty]', )
+
+
+if __name__ == '__main__':
+    fire.Fire(RofiCommands)
