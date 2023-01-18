9779bc60ec0560e02c594b8e6c0972ca0d5ae26b
Tsybulskyi Evhenyi <etsy2900@gmail.com>
2023-01-18 12:08:11 +0200

    webview with python3.8 :=

--- i3/MegadonNotesServer.py
+++ i3/MegadonNotesServer.py
@@ -5,10 +5,9 @@ import i3ipc
 
 i3 = i3ipc.Connection()
 root = i3.get_tree()
-finding = root.find_named('MegadonNotesServer1')
-if finding:
+if finding := root.find_named('MegadonNotesServer1'):
     root.command('workspace Notes')
-    root.command('[con_id=%s] focus' % finding[0].id)
+    root.command(f'[con_id={finding[0].id}] focus')
 else:
     root.command('workspace Notes')
     webview.create_window('MegadonNotesServer1', 'http://91.241.167.37:99/')
