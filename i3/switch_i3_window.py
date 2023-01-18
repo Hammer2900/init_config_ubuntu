af5cd3e0274e4d66690c0b33104e4296fff94bca
Tsybulskyi Evhenyi <etsy2900@gmail.com>
2023-01-18 12:07:01 +0200

    WorkspaceSwitcher

--- i3/switch_i3_window.py
+++ i3/switch_i3_window.py
@@ -10,15 +10,14 @@ def sss(message: str):
 
 
 class WorkspaceSwitcher:
-    """bindsym $mod+Control+z exec "/run/media/izot/652aebd5-b153-4f9e-ba3d-2fb
-    1b4d4b246/jek/TEMP/all_python/i3wm/switch_i3_window.py switch Pycharm
+    """bindsym $mod+Control+z exec "switch_i3_window.py switch Pycharm
     'workspace PyCharm'"."""
 
     def __init__(self):
         self.i3 = i3ipc.Connection()
         self.db = '/dev/shm/window.db'
 
-    def switch(self, tag, command):
+    def switch(self, tag: str, command: str):
         """Switch to previous workspace or exec command."""
         with shelve.open(self.db) as f:
             workspaces = self.i3.get_workspaces()
@@ -34,9 +33,18 @@ class WorkspaceSwitcher:
                 self.i3.command(command)
 
     def center_window(self):
-        """Float and center window.
-
-        :return:
+        """
+        The center_window() function is a method of the WorkspaceSwitcher class that is used to float and center the currently focused window in the i3 window manager.
+        When the method is called, it uses the i3.get_tree().find_focused() method to get the currently focused window.
+        It then checks if the window is already in floating mode (floating == 'user_on'), if so it disables the floating mode by executing the command 'floating disable'.
+        If the window is not in floating mode, the method enables floating mode for the window by executing the command 'floating enable'.
+        Then it gets the name of the workspace where the window is located and the rectangle of the workspace using a.workspace().name and a.workspace().rect respectively.
+        The method then calculates the new width of the window to be half of the width of the workspace and new position of the window by dividing the new width by 2.
+        Then it executes the command 'move position {window_position:.0f} {rect.y + 25}' to move the window to the new position.
+        The method also resizes the window by executing the command 'resize set {new_window_width:.0f} {rect.height - 70}' to set the new width and height.
+        It then focuses the window by executing the command '[con_id="{a.id}"] focus' and switches to the workspace where the window is located by executing the command 'workspace {workspace_name}'.
+        Finally, the method uses the 'xdotool' command to move the mouse cursor to the top-left corner of the window.
+        Note that this function is assuming that the width of the workspace is bigger than its height, otherwise it may not center the window as expected.
         """
         a = self.i3.get_tree().find_focused()
         if a.floating == 'user_on':
