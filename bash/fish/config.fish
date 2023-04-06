00228515fbcd19d2fe7ad0f9c7c575ba276560f9
Tsybulskyi Evhenyi <etsy2900@gmail.com>
2023-04-06 12:22:47 +0300

    Add create-or-remove-symlink function doc string

--- bash/fish/config.fish
+++ bash/fish/config.fish
@@ -2,7 +2,28 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
-# Define the function
+# Finds and activates the first 'activate.fish' script found in any subdirectory
+# under the current directory.
+
+# Usage:
+# find-and-activate
+
+# This function searches for any 'activate.fish' script in any subdirectory
+# under the current directory. When it finds the first such script, it sources
+# it (i.e., runs it in the current shell context) and returns immediately.
+# The purpose of this function is to help set up development environments that
+# require activation scripts to be sourced to add the necessary environment
+# variables and settings.
+
+# Note that this function assumes that each subdirectory contains at most one
+# 'activate.fish' script, and that the first one found is the one to be sourced.
+# If multiple scripts are found, only the first one will be used, and the others
+# will be ignored. If no 'activate.fish' script is found, this function will
+# simply return without doing anything.
+
+# Example usage:
+# $ cd myproject
+# $ find-and-activate
 function find-and-activate
   for dir in (find . -type d)
     if test -f $dir/activate.fish
@@ -12,6 +33,20 @@ function find-and-activate
   end
 end
 
+# create-or-remove-symlink: Creates or removes a symlink to a target file.
+
+# This function takes one argument, which is the path to the target file. It creates a
+# symlink to the target file in /usr/local/bin/ with a name in lowercase letters. If
+# a symlink already exists at that path, it will be removed before creating the new one.
+
+# Parameters:
+#     $argv[1] (string): The path to the target file.
+
+# Returns:
+#     None.
+
+# Example:
+#     $ create-or-remove-symlink /path/to/target/file
 function create-or-remove-symlink
   set target (readlink -f $argv[1])
   set linkname (basename $target | tr '[:upper:]' '[:lower:]')
