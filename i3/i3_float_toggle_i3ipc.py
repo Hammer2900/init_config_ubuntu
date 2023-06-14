8681c4f11d551dd18ca27cacdac737d1e7195ab2
Tsybulskyi Evhenyi <etsy2900@gmail.com>
2023-06-14 17:18:58 +0300

    feat: add floating window toggling
    Add function `adjust_windows` to automatically adjust floating windows upon window focus. When there are only two windows, toggle split direction. When there are no floating windows, create one with a specified size and position. When there are several floating windows, enable and center a focused floating window.

---
+++ i3/i3_float_toggle_i3ipc.py
@@ -0,0 +1,32 @@
+import i3ipc
+
+i3 = i3ipc.Connection()
+
+
+def adjust_windows(workspace):
+    windows = workspace.descendants()
+    floating_windows = [win for win in windows if win.floating == 'user_on']
+    focused = workspace.find_focused()
+
+    if len(windows) == 2:
+        focused.command('layout toggle split')
+    elif len(floating_windows) == 0:
+        focused.command('floating enable, resize set 1000 800, move position center')
+    else:
+        floating_win = floating_windows[0]
+
+        if focused.id != floating_win.id:
+            floating_win.command('floating disable')
+            focused.command('floating enable, resize set 1000 800, move position center')
+
+
+def on_window_focus(i3, event):
+    focused = i3.get_tree().find_focused()
+    workspace = focused.workspace()
+
+    if workspace and workspace.name == '4':
+        adjust_windows(workspace)
+
+
+i3.on(i3ipc.Event.WINDOW_FOCUS, on_window_focus)
+i3.main()
