13d112dc9eb25793c9b94b5ab264db4d1a15bb7a
Tsybulskyi Evhenyi <etsy2900@gmail.com>
2023-06-15 23:27:23 +0300

    size fix

--- i3/i3_float_toggle_i3ipc.py
+++ i3/i3_float_toggle_i3ipc.py
@@ -11,20 +11,20 @@ def adjust_windows(workspace):
     if len(windows) == 2:
         focused.command('layout toggle split')
     elif len(floating_windows) == 0:
-        focused.command('floating enable, resize set 1000 800, move position center')
+        focused.command('floating enable, resize set 1500 800, move position center')
     else:
         floating_win = floating_windows[0]
 
         if focused.id != floating_win.id:
             floating_win.command('floating disable')
-            focused.command('floating enable, resize set 1000 800, move position center')
+            focused.command('floating enable, resize set 1500 800, move position center')
 
 
 def on_window_focus(i3, event):
     focused = i3.get_tree().find_focused()
     workspace = focused.workspace()
 
-    if workspace and workspace.name == '4':
+    if workspace and workspace.name == 'TEXT':
         adjust_windows(workspace)
 
 
